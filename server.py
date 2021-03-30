ead530349fb48ce42b6c52f9caab23cec756f990
Abolo Samuel <ikabolo59@gmail.com>
2021-03-31 02:33:16 +0300

    Fixed syntax error

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ def purchases():
 		data = dict( request.get_json() )
 		ref = data['ref']
 		uid = data['uid']
-		item['quantity'] = 1 if 'quantity' not in item
+		item['quantity'] = 1 if 'quantity' not in item else item['quantity']
 		logs = []
 
 		for item in data['basket']:
